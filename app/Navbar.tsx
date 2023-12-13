ea6bd6aa2624495b9f4a289434b0694c13868daa
thoriq.noer <thoriq.noer@gmail.com>
2023-12-13 10:07:13 +0700

    Fix navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -6,6 +6,8 @@ import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
 import classNames from 'classnames'
 
+import { Container, Box, DropdownMenu, Flex, Text } from '@radix-ui/themes'
+
 const Navbar = () => {
 	const currentPath = usePathname()
 	console.log(currentPath)
@@ -17,25 +19,31 @@ const Navbar = () => {
 
 	return (
 		<nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-			<Link href="/">
-				<AiFillBug />
-			</Link>
-			<ul className='flex space-x-6'>
-				{links.map((link) => (
-					<li key={link.href}>
-					<Link 
-						href={link.href}
-						className={classNames({
-							'text-zinc-900 font-semibold': link.href === currentPath,
-							'text-zinc-500': link.href !== currentPath,
-							'hover:text-zinc-800 transition-colors': true
-						})}
-					>
-						{link.label}
-					</Link>
-				</li>
-				))}
-			</ul>
+			<Container>
+				<Flex justify="between">
+					<Flex align="center" gap="3">
+						<Link href="/">
+							<AiFillBug />
+						</Link>
+						<ul className='flex space-x-6'>
+							{links.map((link) => (
+								<li key={link.href}>
+									<Link
+										href={link.href}
+										className={classNames({
+											'text-zinc-900 font-semibold': link.href === currentPath,
+											'text-zinc-500': link.href !== currentPath,
+											'hover:text-zinc-800 transition-colors': true
+										})}
+									>
+										{link.label}
+									</Link>
+								</li>
+							))}
+						</ul>
+					</Flex>
+				</Flex>
+			</Container>
 		</nav>
 	)
 }
